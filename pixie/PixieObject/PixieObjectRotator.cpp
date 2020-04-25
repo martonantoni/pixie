9fad5a48da23f387f423426799918aa95aee3ae6
Marci <marton.antoni@gmail.com>
2020-04-26 00:22:43 +0200

    rotator with fixed goal

--- pixie/PixieObject/PixieObjectRotator.cpp
+++ pixie/PixieObject/PixieObjectRotator.cpp
@@ -1,21 +1,24 @@
 #include "StdAfx.h"
 
-cPixieObjectRotator::cPixieObjectRotator(unsigned int TimeForFullCircle, eDirection Direction)
+cPixieObjectRotator::cPixieObjectRotator(unsigned int TimeForFullCircle, eDirection Direction, float TargetSumRotation)
 	: cPixieObjectAnimator(DoneWhenSamePropertyAnimatorAdded|DoneWhenAffectedPropertySet, cPixieObject::Property_Rotation)
 	, mTimeForFullCircle(TimeForFullCircle)
 	, mDirection(Direction)
+    , mTargetSumRotation(TargetSumRotation)
 {
 }
 
-cPixieObjectAnimator::eAnimateResult cPixieObjectRotator::Animate(cPixieObject &Object)
+cPixieObjectAnimator::eAnimateResult cPixieObjectRotator::Animate(cPixieObject& Object)
 {
-	auto ElapsedTime=gFrameTime-GetStartTime();
-	auto ElapsedCircleTime=ElapsedTime%mTimeForFullCircle;
-	if(mDirection==ClockWise)
-		Object.SetFloatProperty(cPixieObject::Property_Rotation, mStartRotation+ElapsedCircleTime*360.0/mTimeForFullCircle);
-	else
-		Object.SetFloatProperty(cPixieObject::Property_Rotation, mStartRotation-ElapsedCircleTime*360.0/mTimeForFullCircle);
-	return cPixieObjectAnimator::AnimationActive;
+    auto ElapsedTime = gFrameTime - GetStartTime();
+    float fullRotation = ElapsedTime * 360.0 / mTimeForFullCircle;
+    bool done = fullRotation >= mTargetSumRotation;
+    auto ElapsedCircleTime = ElapsedTime % mTimeForFullCircle;
+    if (mDirection == ClockWise)
+        Object.SetFloatProperty(cPixieObject::Property_Rotation, mStartRotation + (done ? mTargetSumRotation : ElapsedCircleTime * 360.0 / mTimeForFullCircle));
+    else
+        Object.SetFloatProperty(cPixieObject::Property_Rotation, mStartRotation - (done ? mTargetSumRotation : ElapsedCircleTime * 360.0 / mTimeForFullCircle));
+    return done ? cPixieObjectAnimator::AnimationDone : cPixieObjectAnimator::AnimationActive;
 }
 
 void cPixieObjectRotator::Activated(cPixieObject &Object)
--- pixie/PixieObject/PixieObjectRotator.h
+++ pixie/PixieObject/PixieObjectRotator.h
@@ -7,10 +7,16 @@ public:
 private:
 	unsigned int mTimeForFullCircle;
 	float mStartRotation=0.0;
+    float mTargetSumRotation;
 	eDirection mDirection;
 protected:
 	virtual void Activated(cPixieObject &Object) override;
 	virtual eAnimateResult Animate(cPixieObject &Object) override;
 public:
-	cPixieObjectRotator(unsigned int TimeForFullCircle, eDirection Direction);
+	cPixieObjectRotator(unsigned int TimeForFullCircle, eDirection Direction, float TargetSumRotation);
+
+    static tIntrusivePtr<cPixieObjectRotator> CreateEndless(unsigned int TimeForFullCircle, eDirection Direction)
+    {
+        return make_intrusive_ptr<cPixieObjectRotator>(TimeForFullCircle, Direction, -1.0);
+    }
 };
