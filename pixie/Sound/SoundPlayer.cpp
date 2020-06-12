0c2a17a3d14734418eb0f7ba78b9e7306ef97dd5
Marci <marton.antoni@gmail.com>
2020-06-12 14:26:41 +0200

    first time we have sound

--- pixie/Sound/SoundPlayer.cpp
+++ pixie/Sound/SoundPlayer.cpp
@@ -121,13 +121,16 @@ class cRiffFile
         uint64_t mPosition;
     };
     std::vector<cRiffRecord> mRecords;
+    bool mWasError = false;
 public:
     cRiffFile(const cPath& path);
     uint32_t format() const
     {
         return mHeader.format;
     }
-
+    bool operator!() const { return mWasError; }
+    operator bool() const { return !mWasError; }
+    std::vector<char> ReadChunkData(uint32_t chunkId);
 };
 
 cRiffFile::cRiffFile(const cPath& path)
@@ -136,27 +139,65 @@ cRiffFile::cRiffFile(const cPath& path)
     mFile.Read(reinterpret_cast<char*>(&mHeader), sizeof(mHeader));
     mHeader.id = ntohl(mHeader.id);
     mHeader.format = ntohl(mHeader.format);
-    RELEASE_ASSERT(mHeader.id == 'RIFF');
+    if (mHeader.id != 'RIFF')
+    {
+        MainLog->Log("file id is not RIFF: %s", path.c_str());
+        mWasError = true;
+        return;
+    }
     for(;;)
     {
         cRiffRecordHeader recordHeader;
         uint64_t bytesRead = mFile.Read(reinterpret_cast<char*>(&recordHeader), sizeof(recordHeader));
         if (!bytesRead)
             break;
-        
+        MainLog->Log("Found chunk: %.4s", (const char*)&recordHeader.signature);
         mRecords.emplace_back();
         mRecords.back().mLength = recordHeader.length;
         mRecords.back().mSignature = ntohl(recordHeader.signature);
         mRecords.back().mPosition = mFile.GetPosition();
-        mFile.SetPosition(mRecords.back().mLength, cFile::Seek_Cur);
+        mFile.SetPosition((mRecords.back().mLength + 3)&~3, cFile::Seek_Cur);
     }
-
-    RELEASE_ASSERT(!mFile.WasError());
+    mWasError = mFile.WasError();
 }
+
+std::vector<char> cRiffFile::ReadChunkData(uint32_t chunkId) 
+{
+    auto i = std::find_if(ALL(mRecords), [chunkId](auto& record) { return record.mSignature == chunkId; });
+    if (i == mRecords.end())
+        return {};
+    mFile.SetPosition(i->mPosition, cFile::Seek_Set);
+    std::vector<char> data(i->mLength);
+    mFile.Read(data.data(), i->mLength);
+    return data;
+}
+
 
 void cSoundPlayer::Play(const char* filename)
 {
     cRiffFile file(filename);
     
+    RELEASE_ASSERT(file);
+    auto wfxChunkData = file.ReadChunkData('fmt ');
+    auto dataChunkData = file.ReadChunkData('data');
+    if (!wfxChunkData.empty() && !dataChunkData.empty())
+    {
+        XAUDIO2_BUFFER buffer = { 0 };
+        buffer.AudioBytes = dataChunkData.size();  //buffer containing audio data
+        buffer.pAudioData = (const BYTE*)dataChunkData.data();  //size of the audio buffer in bytes
+        buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
 
+        IXAudio2SourceVoice* pSourceVoice;
+        if (FAILED(mXAudio2->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)wfxChunkData.data()))) 
+            return;
+
+        if (FAILED(pSourceVoice->SubmitSourceBuffer(&buffer)))
+            return;
+
+        if (FAILED(pSourceVoice->Start(0)))
+            return;
+
+        mEffects.emplace_back();
+        mEffects.back().mData = std::move(dataChunkData);
+    }
 }
--- pixie/Sound/SoundPlayer.h
+++ pixie/Sound/SoundPlayer.h
@@ -6,6 +6,11 @@ class cSoundPlayer final
 {
     IXAudio2* mXAudio2;
     IXAudio2MasteringVoice* mMasterVoice;
+    struct cEffectData
+    {
+        std::vector<char> mData;
+    };
+    std::vector<cEffectData> mEffects;
 public:
     void Initialize();
     void Play(const char* filaname);
