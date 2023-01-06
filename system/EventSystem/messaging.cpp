8e04b764cf77d2aa2d6108c70dd72bfc464b0895
Marci <marton.antoni@gmail.com>
2023-01-06 11:43:58 +0100

    messaging -- going to abandon this for a while

--- system/EventSystem/messaging.cpp
+++ system/EventSystem/messaging.cpp
@@ -2,7 +2,42 @@
 
 #include "messaging.h"
 
-void cMessageDispatcher::scheduleTopic(MessagingInternal::cMessageTopic* topic)
+cRegisteredID cMessageTopic::registerListener(const cListenerFunction& listeningFunction)
+{
+    return cRegisteredID(this, mListeners.Register(listeningFunction));
+}
+
+void cMessageTopic::dispatchMessages()
+{
+    mIsScheduled = false;
+    std::swap(mMessagesReading, mMessagesWriting);
+    for (auto& message : mMessagesReading)
+    {
+        mListeners.ForEach([&message](auto& listener)
+            {
+                listener(*message);
+            });
+    }
+    mMessagesReading.clear();
+}
+
+void cMessageTopic::Unregister(const cRegisteredID& RegisteredID, eCallbackType CallbackType)
+{
+    mListeners.Unregister(RegisteredID.GetID());
+}
+
+void cMessageTopic::post(cMessagePtr message)
+{
+    mMessagesWriting.emplace_back(std::move(message));
+    if (!mIsScheduled)
+    {
+        mDispatcher->scheduleTopic(this);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+void cMessageDispatcher::scheduleTopic(cMessageTopic* topic)
 {
     mScheduledTopicsWriting.emplace_back(topic);
     if (mScheduledTopicsWriting.size() == 1 && mActivationCallback)
--- system/EventSystem/messaging.h
+++ system/EventSystem/messaging.h
@@ -1,85 +1,43 @@
 #pragma once
 
-namespace MessagingInternal
-{
-
-class cMessageTopic
-{
-public:
-    virtual void dispatchMessages() = 0;
-};
-
-}
-
 class cMessageDispatcher;
 
-template<class MessageType> class tMessageTopic:
-    public tLocatableResourceBase<tMessageTopic<MessageType>>,
-    public cRegistrationHandler,
-    protected MessagingInternal::cMessageTopic
+class cMessageBase
 {
 public:
-    using cListenerFunction = std::function<void(const MessageType& message)>;
-    
-    void post(tIntrusivePtr<MessageType> message);
-    cRegisteredID registerListener(const cListenerFunction& listener);
-    virtual void Unregister(const cRegisteredID& RegisteredID, eCallbackType CallbackType = eCallbackType::Wait) override;
+    virtual ~cMessageBase() = default;
+};
 
+class cMessageTopic:
+    public tLocatableResourceBase<cMessageTopic>,
+    public cRegistrationHandler
+{
+public:
+    using cMessagePtr = std::unique_ptr<const cMessageBase>;
+    using cListenerFunction = std::function<void(const cMessageBase&)>;
+
+    void post(cMessagePtr message);
+    cRegisteredID registerListener(const cListenerFunction& listeningFunction);
+
+    virtual void Unregister(const cRegisteredID& RegisteredID, eCallbackType CallbackType = eCallbackType::Wait) override;
+    void dispatchMessages();
 private:
     std::shared_ptr<cMessageDispatcher> mDispatcher;
     bool mIsScheduled = false;
     tSafeObjects<cListenerFunction> mListeners;
-    std::vector<tIntrusivePtr<MessageType>> mMessagesWriting, mMessagesReading;
-    virtual void dispatchMessages() override;
+    std::vector<cMessagePtr> mMessagesWriting, mMessagesReading;
 };
 
 class cMessageDispatcher
 {
 public:
     using cActivationCallback = std::function<void(cMessageDispatcher&)>;
-    void scheduleTopic(MessagingInternal::cMessageTopic* topic);
+    void scheduleTopic(cMessageTopic* topic);
     void dispatchAll();
     void setActivationCallback(const cActivationCallback& callback);
 private:
     cActivationCallback mActivationCallback;
-    std::vector<MessagingInternal::cMessageTopic*> mScheduledTopicsWriting, mScheduledTopicsReading;
+    std::vector<cMessageTopic*> mScheduledTopicsWriting, mScheduledTopicsReading;
 };
 
 
-template<class MessageType>
-cRegisteredID tMessageTopic<MessageType>::registerListener(const cListenerFunction& listener)
-{
-    return cRegisteredID(this, mListeners.Register(cListenerFunction));
-}
-
-template<class MessageType>
-void tMessageTopic<MessageType>::dispatchMessages()
-{
-    mIsScheduled = false;
-    std::swap(mMessagesReading, mMessageWriting);
-    for (auto& message : mMessagesReading)
-    {
-        mListeners.ForEach([&message](auto& listener)
-        {
-            listener(*message);
-        });
-    }
-    mMessagesReading.clear();
-}
-
-template<class MessageType>
-void tMessageTopic<MessageType>::Unregister(const cRegisteredID& RegisteredID, eCallbackType CallbackType)
-{
-    mListeners.Unregister(RegisteredID.GetID());
-}
-
-
-template<class MessageType> 
-void tMessageTopic<MessageType>::post(tIntrusivePtr<MessageType> message)
-{
-    mMessagesWriting.emplace_back(std::move(message));
-    if (!mIsScheduled)
-    {
-        mDispatcher->scheduleTopic(this);
-    }
-}
