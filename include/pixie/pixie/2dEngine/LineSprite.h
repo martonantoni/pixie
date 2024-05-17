5996e4d90679e7a5fc40c4db7374aa591c6e5829
Marci <marton.antoni@gmail.com>
2024-05-17 12:48:26 +0200

    linesprite width

--- include/pixie/pixie/2dEngine/LineSprite.h
+++ include/pixie/pixie/2dEngine/LineSprite.h
@@ -1,4 +1,4 @@
 #pragma once
 
-void SpriteBetweenPoints(cSprite &Sprite, cPoint a, cPoint b);
-std::unique_ptr<cSprite> Create1PixLineSprite(cPoint a, cPoint b);
+void SpriteBetweenPoints(cSprite &Sprite, cPoint a, cPoint b, int width = 1);
+std::unique_ptr<cSprite> CreateLineSprite(cPoint a, cPoint b, int width = 1);
--- src/pixie/2dEngine/LineSprite.cpp
+++ src/pixie/2dEngine/LineSprite.cpp
@@ -1,7 +1,7 @@
 #include "StdAfx.h"
 #include "pixie/pixie/i_pixie.h"
 
-void SpriteBetweenPoints(cSprite &Sprite, cPoint a, cPoint b)
+void SpriteBetweenPoints(cSprite &Sprite, cPoint a, cPoint b, int width)
 {
 	auto dx=a.x-b.x;
 	auto dy=a.y-b.y;
@@ -9,19 +9,14 @@ void SpriteBetweenPoints(cSprite &Sprite, cPoint a, cPoint b)
 	auto Angle=dx?90.0+atan((double)dy/(double)dx)*180.0/3.141592653589793238462643383279:0.0;
 	cPoint Center=cPoint(a.x+b.x, a.y+b.y)/2;
 	cPoint StartPoint=Center-cPoint(0, Length/2);
-	Sprite.SetPlacement(cRect(StartPoint, { 1, (int)Length }));
+	Sprite.SetPlacement(cRect(StartPoint, { width, (int)Length }));
 	Sprite.SetRotation(Angle);
 }
 
-std::unique_ptr<cSprite> Create1PixLineSprite(cPoint a, cPoint b)
+std::unique_ptr<cSprite> CreateLineSprite(cPoint a, cPoint b, int width)
 {
-	{
-		const std::unique_ptr<cPixieWindow> Window=std::make_unique<cPixieWindow>();
-		Window->SetPlacement({ 20,20,30,30 });
-	}
-
 	auto LineSprite=std::make_unique<cSprite>();
 	LineSprite->SetTexture(theTextureManager.GetTexture("1pix"));
-	SpriteBetweenPoints(*LineSprite, a, b);
+	SpriteBetweenPoints(*LineSprite, a, b, width);
 	return LineSprite;
 }
