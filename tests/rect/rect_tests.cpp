f4b5698f806241a50ccef6dc673afc61e9a50bfb
Marci <marton.antoni@gmail.com>
2024-09-05 21:51:07 +0200

    working on the new Rect

--- tests/rect/rect_tests.cpp
+++ tests/rect/rect_tests.cpp
@@ -2,9 +2,21 @@
 
 #include "pixie/system/CommonInclude_System.h"
 
+enum class eVerticalAlign { Top, Bottom, Center };
+enum class eHorizontalAlign { Left, Right, Center };
+
+template<class T> concept cPoints = requires(T t)
+{
+    t.begin();
+    t.end();
+    t.empty();
+    *t.begin();
+    cPoint(*t.begin());
+};
+
 class cRect2;
 
-template<class OWNER, class VALUE_TYPE, auto getter, auto setter> class tProxy
+template<class OWNER, class VALUE_TYPE, auto getter, auto setter> class tProxy final
 {
     OWNER& mOwner;
 public:
@@ -23,7 +35,7 @@ public:
     bool operator!=(VALUE_TYPE value) const { return getter(mOwner) != value; }
 };
 
-class cRect2
+class cRect2 final
 {
     cPoint mTopLeft;
     cPoint mSize;
@@ -50,79 +62,205 @@ public:
     int right() const { return mTopLeft.x + mSize.x; }
     int bottom() const { return mTopLeft.y + mSize.y; }
 
-    auto bottomRight();
-    auto bottomLeft();
+
     auto centerPosition();
     auto width();
     auto height();
-    auto left();
-    auto top();
-    auto right();
-    auto bottom();
+
+    struct PreserveSize {};
+
+    template<class... Properties> auto bottomRight();
+    template<class... Properties> auto bottomLeft();
+    template<class... Properties> auto left();
+    template<class... Properties> auto top();
+    template<class... Properties> auto right();
+    template<class... Properties> auto bottom();
+
+    enum class eIsOptional { Yes, No };
+    void fromConfig(const cConfig& config, eIsOptional isOptional = eIsOptional::No);
+    void toConfig(cConfig& config) const;
+
+    void growToBound(const cRect2& rectToBound);
+    void growToBound(const cPoint& point);
+
+    static cRect2 aroundPoint(cPoint point, cPoint size);
+    static cRect2 alignedRect(const cRect2& baseRect, const cRect2& rectToAlign, eHorizontalAlign horizontalAlign, eVerticalAlign verticalAlign);
+    static cRect2 centeredRect(const cRect2& baseRect, const cRect2& rectToCenter);
+    template<cPoints T> static cRect2 boundingBox(const T& points);
 };
 
-auto cRect2::bottomRight()
-{
-    return tProxy<cRect2, cPoint,
-        [](auto& rect) { return rect.mTopLeft + rect.mSize; },
-        [](auto& rect, auto value) { rect.mSize = value - rect.mTopLeft; }> (*this);
-}
-
-auto cRect2::bottomLeft()
-{
-    return tProxy<cRect2, cPoint,
-        [](auto& rect) { return rect.mTopLeft + cPoint(0, rect.mSize.y); },
-        [](auto& rect, auto value) { rect.mSize.y = value.y - rect.mTopLeft.y; rect.mTopLeft.x = value.x; }> (*this);
-}
 
 auto cRect2::centerPosition()
 {
-    return tProxy<cRect2, cPoint,
+    return tProxy < cRect2, cPoint,
         [](auto& rect) { return rect.mTopLeft + rect.mSize / 2; },
-        [](auto& rect, auto value) { rect.mTopLeft = value - rect.mSize / 2; }> (*this);
+        [](auto& rect, auto value) { rect.mTopLeft = value - rect.mSize / 2; } > (*this);
 }
 
 auto cRect2::width()
 {
-    return tProxy<cRect2, int,
+    return tProxy < cRect2, int,
         [](auto& rect) { return rect.mSize.x; },
-        [](auto& rect, auto value) { rect.mSize.x = value; }> (*this);
+        [](auto& rect, auto value) { rect.mSize.x = value; } > (*this);
 }
 
 auto cRect2::height()
 {
-    return tProxy<cRect2, int,
+    return tProxy < cRect2, int,
         [](auto& rect) { return rect.mSize.y; },
-        [](auto& rect, auto value) { rect.mSize.y = value; }> (*this);
+        [](auto& rect, auto value) { rect.mSize.y = value; } > (*this);
 }
 
-auto cRect2::left()
+
+template<class... Properties> auto cRect2::bottomRight()
 {
-    return tProxy<cRect2, int,
-        [](auto& rect) { return rect.mTopLeft.x; },
-        [](auto& rect, auto value) { rect.mSize.x += rect.mTopLeft.x - value; rect.mTopLeft.x = value; }> (*this);
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, cPoint,
+            [](auto& rect) { return rect.mTopLeft + rect.mSize; },
+            [](auto& rect, auto value) { rect.mTopLeft = value - rect.mSize; }> (*this);
+    }
+    else
+    {
+        return tProxy<cRect2, cPoint,
+            [](auto& rect) { return rect.mTopLeft + rect.mSize; },
+            [](auto& rect, auto value) { rect.mSize = value - rect.mTopLeft; }> (*this);
+    }
 }
 
-auto cRect2::top()
+template<class... Properties> auto cRect2::bottomLeft()
 {
-    return tProxy<cRect2, int,
-        [](auto& rect) { return rect.mTopLeft.y; },
-        [](auto& rect, auto value) { rect.mSize.y += rect.mTopLeft.y - value; rect.mTopLeft.y = value; }> (*this);
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, cPoint,
+            [](auto& rect) { return rect.mTopLeft + cPoint(0, rect.mSize.y); },
+            [](auto& rect, auto value) { rect.mTopLeft = value - cPoint(0, rect.mSize.y); }> (*this);
+    }
+    else
+    {
+        return tProxy<cRect2, cPoint,
+            [](auto& rect) { return rect.mTopLeft + cPoint(0, rect.mSize.y); },
+            [](auto& rect, auto value) // top right corner is fixed
+            { 
+                rect.mSize.x += rect.mTopLeft.x - value.x; 
+                rect.mTopLeft.x = value.x;
+                rect.mSize.y = value.y - rect.mTopLeft.y;
+            }> (*this);
+    }
 }
 
-auto cRect2::right()
+template<class... Properties> auto cRect2::left()
 {
-    return tProxy<cRect2, int,
-        [](auto& rect) { return rect.mTopLeft.x + rect.mSize.x; },
-        [](auto& rect, auto value) { rect.mSize.x = value - rect.mTopLeft.x; }> (*this);
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.x; },
+            [](auto& rect, auto value) { rect.mTopLeft.x = value; }> (*this);
+    }
+    else
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.x; },
+            [](auto& rect, auto value) { rect.mSize.x += rect.mTopLeft.x - value; rect.mTopLeft.x = value; }> (*this);
+    }
 }
 
-auto cRect2::bottom()
+template<class... Properties> auto cRect2::top()
 {
-    return tProxy<cRect2, int,
-        [](auto& rect) { return rect.mTopLeft.y + rect.mSize.y; },
-        [](auto& rect, auto value) { rect.mSize.y = value - rect.mTopLeft.y; }> (*this);
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.y; },
+            [](auto& rect, auto value) { rect.mTopLeft.y = value; }> (*this);
+    }
+    else
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.y; },
+            [](auto& rect, auto value) { rect.mSize.y += rect.mTopLeft.y - value; rect.mTopLeft.y = value; }> (*this);
+    }
 }
+
+template<class... Properties> auto cRect2::right()
+{
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.x + rect.mSize.x; },
+            [](auto& rect, auto value) { rect.mSize.x = value - rect.mTopLeft.x; }> (*this);
+    }
+    else
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.x + rect.mSize.x; },
+            [](auto& rect, auto value) { rect.mTopLeft.x = value - rect.mSize.x; }> (*this);
+    }
+}
+
+template<class... Properties> auto cRect2::bottom()
+{
+    if constexpr (std::disjunction_v<std::is_same<Properties, PreserveSize>...>)
+    {
+        return tProxy<cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.y + rect.mSize.y; },
+            [](auto& rect, auto value) { rect.mSize.y = value - rect.mTopLeft.y; }> (*this);
+    }
+    else
+    {
+        return tProxy < cRect2, int,
+            [](auto& rect) { return rect.mTopLeft.y + rect.mSize.y; },
+            [](auto& rect, auto value) { rect.mSize.y = value - rect.mTopLeft.y; } > (*this);
+    }
+}
+
+void cRect2::fromConfig(const cConfig& config, eIsOptional isOptional)
+{
+    if (isOptional == eIsOptional::Yes)
+    {
+        mTopLeft = { config.get<int>("x", left()), config.get<int>("y", top()) };
+        mSize = { config.get<int>("w", width()), config.get<int>("h", height()) };
+    }
+    else
+    {
+        mTopLeft = { config.get<int>("x"), config.get<int>("y") };
+        mSize = { config.get<int>("w"), config.get<int>("h") };
+    }
+}
+
+void cRect2::toConfig(cConfig& config) const
+{
+    config.set("x"s, left());
+    config.set("y"s, top());
+    config.set("w"s, width());
+    config.set("h"s, height());
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// creation
+
+
+cRect2 cRect2::alignedRect(const cRect2& baseRect, const cRect2& RectToAlign, eHorizontalAlign HorizontalAlign, eVerticalAlign VerticalAlign)
+{
+    return {};
+}
+
+cRect2 cRect2::aroundPoint(cPoint point, cPoint size)
+{ 
+    return cRect2(point - size / 2, size); 
+}
+
+
+cRect2 cRect2::centeredRect(const cRect2& baseRect, const cRect2& rectToCenter)
+{
+    return alignedRect(baseRect, rectToCenter, eHorizontalAlign::Center, eVerticalAlign::Center);
+}
+
+template<cPoints T> cRect2 cRect2::boundingBox(const T & points)
+{
+    return {};
+}
+
 
 inline std::ostream& operator<<(std::ostream& os, const cPoint& point)
 {
@@ -281,7 +419,61 @@ TEST(Rect, left)
     testedRect.left() = 2;  // set
     EXPECT_EQ(testedRect.left(), 2);
     EXPECT_EQ(testedRect.position(), cPoint(2, 2)); // check that only left is changed
-    EXPECT_EQ(testedRect.size(), cPoint(
+}
+
+TEST(Rect, bottomRight)
+{
+    cRect2 testedRect(1, 2, 3, 4); // left, top, width, height
+
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(4, 6)); // get
+
+    testedRect.bottomRight() = cPoint(5, 7);  // set
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(5, 7));
+    EXPECT_EQ(testedRect.position(), cPoint(1, 2)); // must not change position
+    EXPECT_EQ(testedRect.size(), cPoint(4, 5)); 
+
+    testedRect.bottomRight() += cPoint(1, 2);  // get + set
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(6, 9));
+    EXPECT_EQ(testedRect.position(), cPoint(1, 2)); // must not change position
+    EXPECT_EQ(testedRect.size(), cPoint(5, 7)); // check that only width and height are changed
+
+}
+
+TEST(Rect, bottomRight_preserveSize)
+{
+    cRect2 testedRect(1, 2, 3, 4); // left, top, width, height
+
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(4, 6)); // get
+
+    testedRect.bottomRight<cRect2::PreserveSize>() = cPoint(5, 7);  // set
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(5, 7));
+    EXPECT_EQ(testedRect.position(), cPoint(2, 3)); // check that position is changed
+    EXPECT_EQ(testedRect.size(), cPoint(3, 4)); // check that size is not changed
+
+    testedRect.bottomRight<cRect2::PreserveSize>() += cPoint(1, 2);  // get + set
+    EXPECT_EQ(testedRect.bottomRight(), cPoint(6, 9));
+    EXPECT_EQ(testedRect.position(), cPoint(3, 5)); // check that position is changed
+    EXPECT_EQ(testedRect.size(), cPoint(3, 4)); // check that size is not changed
+}
+
+TEST(Rect, bottomLeft) 
+{
+    // topRight stays in place
+
+    cRect2 testedRect(1, 2, 3, 4); // left, top, width, height
+
+    EXPECT_EQ(testedRect.bottomLeft(), cPoint(1, 6)); // get
+
+    testedRect.bottomLeft() = cPoint(2, 7);  // set
+    EXPECT_EQ(testedRect.bottomLeft(), cPoint(2, 7));
+    EXPECT_EQ(testedRect.position(), cPoint(2, 2));
+    EXPECT_EQ(testedRect.size(), cPoint(2, 5));
+    EXPECT_EQ(testedRect.topRight(), cPoint(4, 2));
+
+    testedRect.bottomLeft() += cPoint(1, 2);  // get + set
+    EXPECT_EQ(testedRect.bottomLeft(), cPoint(3, 9));
+    EXPECT_EQ(testedRect.position(), cPoint(3, 2));
+    EXPECT_EQ(testedRect.size(), cPoint(1, 7));
 }
 
 } // namespace RectTests
