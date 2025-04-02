a78f4af4fed6238aba47cfeccf0d01e7ffe3f129
Marci <marton.antoni@gmail.com>
2025-04-02 16:45:27 +0200

    fix TextureAnimator when using texture ptrs instead of strings

--- src/pixie/2dEngine/TextureAnimator.cpp
+++ src/pixie/2dEngine/TextureAnimator.cpp
@@ -7,12 +7,15 @@ void cTextureAnimator::Activated(cPixieObject &Object)
 
 cPixieObjectAnimator::eAnimateResult cTextureAnimator::Animate(cPixieObject &Object)
 {
+    auto sprite = dynamic_cast<cSprite*>(&Object);
+    if (!sprite)
+        return cPixieObjectAnimator::AnimationDone;
 	auto Elapsed=gFrameTime-GetStartTime();
 	auto Frame=(Elapsed*mFramesPerSec)/1000;
 	if(Frame!=mPreviousFrame)
 	{
 		mPreviousFrame=Frame;
-		int numberOfTextures = std::visit([&Object, Frame, this](auto&& textures) -> int
+		int numberOfTextures = std::visit([&Object, Frame, sprite, this](auto&& textures) -> int
 			{
                 if constexpr (std::is_same_v<std::monostate, std::decay_t<decltype(textures)>>)
                     return 0;
@@ -24,11 +27,11 @@ cPixieObjectAnimator::eAnimateResult cTextureAnimator::Animate(cPixieObject &Obj
 				}
 				else if constexpr (std::is_same_v<std::vector<tIntrusivePtr<cTexture>>, std::decay_t<decltype(textures)>>)
 				{
+                    sprite->SetTexture(textures[std::min<size_t>(textures.size() - 1, Frame)]);
 					return textures.size();
 				}
 				return 0;
             }, mTextures);
-//		Object.SetStringProperty(cPixieObject::Property_Texture, mTextureNames[std::min<size_t>(mTextureNames.size()-1, Frame)]);
 		if(Frame>= numberOfTextures)
 			return cPixieObjectAnimator::AnimationDone;
 	}
