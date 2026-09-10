7ad5052a388ecf3a2eebaa94d9addb2b77441394
Marci <marton.antoni@gmail.com>
2026-09-10 14:02:36 +0200

    fixed wrong main log filename

--- src/system/Log/MainLog.cpp
+++ src/system/Log/MainLog.cpp
@@ -2,18 +2,18 @@
 
 cMainLog *MainLog=NULL;
 
-cMainLog::cMainLog(int flags): 
-    cLog(ProgramArguments?
-        ProgramArguments->GetString("MainLogFile",std::format("{}-{}.log",ProgramName,TodayString)):
-        std::format("{}-{}.log",ProgramName,TodayString)
-        ,flags)
+cMainLog::cMainLog(int flags) :
+    cLog(ProgramArguments ?
+        ProgramArguments->GetString("MainLogFile", std::format("{}-{}.log", ProgramName, TodayString.c_str())) :
+        std::format("{}-{}.log", ProgramName, TodayString.c_str())
+        , flags)
 {
 }
 
-cMainLog::cMainLog(): 
-	cLog(ProgramArguments?
-		ProgramArguments->GetString("MainLogFile",std::format("{}-{}.log",ProgramName,TodayString)):
-		std::format("{}-{}.log",ProgramName,TodayString)
-		,cLog::Flags::USE_MUTEX|cLog::Flags::TIME_STAMP|cLog::Flags::ECHO|cLog::Flags::FLUSH_OVER_TIME)
+cMainLog::cMainLog() :
+    cLog(ProgramArguments ?
+        ProgramArguments->GetString("MainLogFile", std::format("{}-{}.log", ProgramName, TodayString.c_str())) :
+        std::format("{}-{}.log", ProgramName, TodayString.c_str())
+        , cLog::Flags::USE_MUTEX | cLog::Flags::TIME_STAMP | cLog::Flags::ECHO | cLog::Flags::FLUSH_OVER_TIME)
 {
 }
--- src/system/TodayString.cpp
+++ src/system/TodayString.cpp
@@ -6,8 +6,12 @@ cTodayString TodayString;
 
 cTodayString::cTodayString()
 {
-	time_t CurrentTime=time(0);
+	time_t CurrentTime = time(0);
 	tm LocalTime;
-	localtime_s(&LocalTime,&CurrentTime);
-	std::string::operator=(std::format("{:02d}-{:02d}-{:02d}",LocalTime.tm_mon+1,LocalTime.tm_mday,LocalTime.tm_year%100));
+	localtime_s(&LocalTime, &CurrentTime);
+	std::string::operator=(
+		std::format("{:02d}-{:02d}-{:02d}",
+			LocalTime.tm_mon + 1,
+			LocalTime.tm_mday,
+			LocalTime.tm_year % 100));
 }
